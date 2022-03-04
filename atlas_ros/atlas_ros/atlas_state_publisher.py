efbb89eddbd81953e38bf8695eee10da4e5d3238
Maxx Wilson <jessemaxxwilson@utexas.edu>
2022-03-04 12:48:36 -0600

    adds atlas_ros ros package adds build, install, log to gitignore adds build script for atlas_ros pkg

---
+++ atlas_ros/atlas_ros/atlas_state_publisher.py
@@ -0,0 +1,29 @@
+
+import rclpy
+from rclpy.node import Node
+
+from collections import OrderedDict
+from sensor_msgs.msg import JointState
+
+class AtlasStatePublisher():
+    def __init__(self):
+        rclpy.init()
+        self.node = Node("atlas_state_publisher")
+        self.js_pub = self.node.create_publisher(JointState, "/joint_states", 10)
+        
+    def publish_robot_state(self, robot_state):
+        newMsg = JointState()
+        joint_pos = robot_state.get("joint_pos")
+        joint_vel = robot_state.get("joint_vel")
+        
+        for key in joint_pos:
+            newMsg.name.append(key)
+            newMsg.position.append(joint_pos.get(key))
+            newMsg.velocity.append(joint_vel.get(key))
+            
+        self.js_pub.publish(newMsg)
+            
+    def shutdown(self):
+        self.node.destroy_node()
+        rclpy.shutdown()
+        
---
+++ scripts/build_atlas_ros.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+colcon build --base-paths atlas_ros
+source ~/.bashrc
