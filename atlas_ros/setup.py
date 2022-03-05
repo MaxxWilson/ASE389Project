a634376157883cc2780f4da2916b8d8b9bf7e00a
Maxx Wilson <jessemaxxwilson@utexas.edu>
2022-03-04 21:05:20 -0600

    atlas URDF directory structure copies properly to install dir

--- atlas_ros/setup.py
+++ atlas_ros/setup.py
@@ -1,16 +1,25 @@
 from setuptools import setup
+import os
 
 package_name = 'atlas_ros'
+
+urdf_files = []
+for root, directory, file in os.walk(os.getcwd() + "/resource/atlas"):
+    for f in file:
+        file_rel_path = os.path.join(os.path.relpath(root, os.getcwd()), f)
+        install_dir = "share/" + package_name + "/" + os.path.relpath(root, os.getcwd())
+        urdf_files.append((install_dir, [file_rel_path]))
+
+data_files = [
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ] + urdf_files
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jwilson',
